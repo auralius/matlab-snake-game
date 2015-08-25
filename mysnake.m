4801aca3ae56e56fe5dd247b0123b869a542fdfd
Auralius Manurung <manurung.auralius@gmail.com>
2015-08-25 03:08:35 +0200

    added collision detection.

--- mysnake.m
+++ mysnake.m
@@ -1,22 +1,40 @@
 % Snake game by Auralius Manurung
 % manurung.auralius@gmail.com
+% 25.08.2015
 %
-
+% Structure of the snake
+%   snake.collision => flag for collision (1 or 0)
+%   snake.running => set to 0 when program is terminated
+%   snake.xmax => world length
+%   snake.ymax => world height
+%   snake.fig_hnd => figure handle
+%   snake.plot_hnd => a plot handle for the snake
+%   snake.plot_head_hnd => a plot handle for head of the snake
+%   snake.segments => N x 2 where N is the length of the snake
+%   snake.dir = {'E', 'S', 'W', 'N'}
+%
 %%
 function mysnake()
     close all;    
     clc;    
     
     snake.running = 1;
+    snake.collision = 0;
+    
+    % Snake world size
+    snake.xmax = 100;
+    snake.ymax = 100;
     
     % Prepare a figure window    
     snake.fig_hnd = figure();
     hold on;
-    snake.plot_hnd = plot(0, 0, 'LineWidth', 4);            
-    snake.plot_head_hnd = plot(0, 0, 'ro');           
+    snake.plot_hnd = plot(0, 0, 'b', 'LineWidth', 4);            
+    snake.plot_head_hnd = plot(0, 0, 'bo');           
             
     % Initial snakes segments and direction
-    snake.segments = [40 40; 39 40; 38 40];
+    snake.segments = [snake.xmax/2   snake.ymax/2; ...
+                      snake.xmax/2-1 snake.ymax/2; ...
+                      snake.xmax/2-2 snake.ymax/2];
     snake.dir = 'E';    
     
         
@@ -24,27 +42,40 @@ function mysnake()
     guidata(snake.fig_hnd, snake);
         
     % Initial drawing    
-    init_figure();    
+    init_figure(snake);    
     draw_snake(snake); 
     
+    % Introduction massage
     intro();
+    
+    %
+    % Your AI here
+    % Use turn_left, turn_right, and forward functions here
+    % You can disable key_pressed_fcn callback function
+    %
 end
 
 %%
-function init_figure()
+function init_figure(snake)
     set (gcf, 'keypressfcn', @(src,eventdata)key_pressed_fcn(src, eventdata));        
-    xlim([0, 80]);
-    ylim([0, 80]);
+    xlim([0, snake.xmax]);
+    ylim([0, snake.ymax]);
     title('mysnake v1.0')
 end
 
 %%
-function draw_snake(snake)
+function draw_snake(snake)    
     set(snake.plot_hnd, 'XData', snake.segments(:,1));
     set(snake.plot_hnd, 'YData', snake.segments(:,2));
     
     set(snake.plot_head_hnd, 'XData', snake.segments(1,1));
     set(snake.plot_head_hnd, 'YData', snake.segments(1,2));
+    
+    if snake.collision == 1
+        set(snake.plot_head_hnd, 'Color', 'r');
+    else
+        set(snake.plot_head_hnd, 'Color', 'b');
+    end
 end
 
 %%
@@ -87,6 +118,15 @@ function snake = forward(snake)
 end
 
 %%
+function snake = check_collision(snake)
+    snake.collision = 0;
+    r = sum(snake.segments(2:end, 1) == snake.segments(1, 1) & snake.segments(2:end, 2) == snake.segments(1, 2));
+    if r > 0
+        snake.collision = 1;
+        disp('collision');
+    end
+end
+%%
 function snake = increase(snake)
     v = snake.segments(end, :) - snake.segments(end-1, :);    
     snake.segments(end+1,:) = snake.segments(end,:) + v;
@@ -106,9 +146,7 @@ function [] = key_pressed_fcn(H, E)
                 snake = turn_left(snake);                
             elseif (snake.dir == 'E')
                 snake = forward(snake);
-            end
-            draw_snake(snake);
-            
+            end                        
             
         case 'leftarrow'
             if (snake.dir == 'N')
@@ -119,8 +157,7 @@ function [] = key_pressed_fcn(H, E)
                 snake = turn_right(snake);               
             elseif (snake.dir == 'W')
                 snake = forward(snake);
-            end
-            draw_snake(snake);
+            end            
             
         case 'uparrow'
             if (snake.dir == 'E')
@@ -131,8 +168,7 @@ function [] = key_pressed_fcn(H, E)
                 snake = turn_right(snake);                
             elseif (snake.dir == 'N')
                 snake = forward(snake);
-            end
-            draw_snake(snake);            
+            end            
             
         case 'downarrow'            
             if (snake.dir == 'E')
@@ -143,12 +179,10 @@ function [] = key_pressed_fcn(H, E)
                 snake = turn_left(snake);            
             elseif (snake.dir == 'S')
                 snake = forward(snake);
-            end
-            draw_snake(snake);
+            end            
             
         case 'space'
-            snake = increase(snake);
-            draw_snake(snake)
+            snake = increase(snake);            
                         
         case 'escape'        
             snake.running = 0;            
@@ -159,6 +193,8 @@ function [] = key_pressed_fcn(H, E)
     end
     
     if snake.running == 1
+        snake = check_collision(snake);
+        draw_snake(snake);
         guidata(snake.fig_hnd, snake);
     end
 end
